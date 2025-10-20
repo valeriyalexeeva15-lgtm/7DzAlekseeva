4d4797ed64ef01da182f0bb1ed2c1872c7aa4f3a
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-20 20:52:14 +0300

    3 car

---
+++ 7DzAlekseeva/Car.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DzAlekseeva
+{
+    internal class Car
+    {
+        public string Model;
+        public int Speed;
+        public int MaxSpeed;
+
+        public Car(string model, int maxSpeed)
+        {
+            Model = model;
+            MaxSpeed = maxSpeed;
+            Speed = 0;
+        }
+
+        public void Accelerate(int delta)
+        {
+            Speed += delta;
+            if (Speed > MaxSpeed)
+            {
+                Speed = MaxSpeed;
+            }
+        }
+
+        public void Brake()
+        {
+            Speed = 0;
+        }
+    }
+}
--- 7DzAlekseeva/Program.cs
+++ 7DzAlekseeva/Program.cs
@@ -4,20 +4,32 @@
     {
         static void Main(string[] args)
         {
+            //1
             //Book book1 = new Book();
             //Book book2 = new Book("Война и мир", "Толстой", 1869, true);
             //book1.PrintInfo();
             //book2.PrintInfo();
-            BankAccount1 account = new BankAccount1("123456789");
 
-            //Пополняем 1000₽
-            account.Deposit(1000);
+            //2
+            //BankAccount1 account = new BankAccount1("123456789");
 
-            //Снимаем 500₽
-            account.Withdraw(500);
+            ////Пополняем 1000₽
+            //account.Deposit(1000);
 
-            //Выводим баланс
-            account.PrintBalance();
+            ////Снимаем 500₽
+            //account.Withdraw(500);
+
+            ////Выводим баланс
+            //account.PrintBalance();
+
+            //3
+            Car car = new Car("Tesla Model S", 250);
+
+            car.Accelerate(120);
+            Console.WriteLine($"Модель: {car.Model}, Текущая скорость: {car.Speed} км/ч");
+
+            car.Brake();
+            Console.WriteLine($"После торможения: {car.Speed} км/ч");
         }
     }
 }
