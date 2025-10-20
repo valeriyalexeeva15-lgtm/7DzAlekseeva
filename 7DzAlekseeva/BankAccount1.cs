40445510199e6386f5a8b6486981953ad6f0ce91
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-20 19:47:01 +0300

    2 BankAcccount

---
+++ 7DzAlekseeva/BankAccount1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DzAlekseeva
+{
+    internal class BankAccount1
+    {
+        public string AccountNumber;
+        private decimal Balance;  // приват. поле
+
+        //пополнение счета
+        public void Deposit(decimal amount)
+        {
+            this.Balance = this.Balance + amount;
+        }
+
+        //снятие денег
+        public void Withdraw(decimal amount)
+        {
+            if (amount <= this.Balance)  //хватает ли средств
+            {
+                this.Balance = this.Balance - amount;
+            }
+            else
+            {
+                Console.WriteLine("Недостаточно средств!");
+            }
+        }
+
+        //вывод баланса
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Текущий баланс: {this.Balance}р");
+        }
+
+        public BankAccount1(string accountNumber)
+        {
+            this.AccountNumber = accountNumber;
+            this.Balance = 0;
+        }
+    }
+}
--- 7DzAlekseeva/Program.cs
+++ 7DzAlekseeva/Program.cs
@@ -4,10 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book();
-            Book book2 = new Book("Война и мир", "Толстой", 1869, true);
-            book1.PrintInfo();
-            book2.PrintInfo();
+            //Book book1 = new Book();
+            //Book book2 = new Book("Война и мир", "Толстой", 1869, true);
+            //book1.PrintInfo();
+            //book2.PrintInfo();
+            BankAccount1 account = new BankAccount1("123456789");
+
+            //Пополняем 1000₽
+            account.Deposit(1000);
+
+            //Снимаем 500₽
+            account.Withdraw(500);
+
+            //Выводим баланс
+            account.PrintBalance();
         }
     }
 }
