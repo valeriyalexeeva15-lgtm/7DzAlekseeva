8a367109bf645b14b99e59d2edad028756283f75
valeriyalexeeva15-lgtm <lera.alekseeva.4240@mail.ru>
2025-10-20 18:42:31 +0300

    Add project files.

---
+++ 7DzAlekseeva/Book.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7DzAlekseeva
+{
+    internal class Book
+    {
+        public string Title;
+        public string Author;
+        public int Year;
+        public bool IsAvailable;
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Название: \"{Title}\", Автор: {Author}, Год: {Year}, Доступна: {(IsAvailable ? "Да" : "Нет")}");
+        }
+        public Book(string title, string author, int year, bool isAvailable)
+        {
+            Title = title;
+            Author = author;
+            Year = year;
+            IsAvailable = isAvailable;
+        }
+        public Book()
+        {
+            Title = "Неизвестно";
+            Author = "Неизвестен";
+            Year = 0;
+            IsAvailable = false;
+        }
+    }
+}
---
+++ 7DzAlekseeva/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace _7DzAlekseeva
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Book book2 = new Book("Война и мир", "Толстой", 1869, true);
+            book1.PrintInfo();
+            book2.PrintInfo();
+        }
+    }
+}
